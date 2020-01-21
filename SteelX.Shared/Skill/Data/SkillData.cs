33113167a0fdd24afb14c7956215697195dd383b
Herbert Milhomme <herb@herbertmilhomme.com>
2020-01-21 14:46:20 -0500

    Update SkillData.cs

--- SteelX.Shared/Skill/Data/SkillData.cs
+++ SteelX.Shared/Skill/Data/SkillData.cs
@@ -2,20 +2,20 @@
 {
 	public struct SkillData
 	{
-		public string Image { get; private set; }
-		//public string Name    		{ get; private set; }
-		public string LeftHand { get; private set; }
-		public string RightHand { get; private set; }
-		public string Level { get; private set; }
-		public string Price { get; private set; }
-		public string RequiredRank { get; private set; }
-		public string Period { get; private set; }
-		public string Damage { get; private set; }
-		public string Radius { get; private set; }
-		public string Range { get; private set; }
-		public string RequiredSP { get; private set; }
-		public int CoolDown { get; private set; }
-		public Skills Id { get; private set; }
+		public int Image			{ get; private set; }
+		public string Name			{ get; private set; }
+		public string Weap1			{ get; private set; }
+		public string Weap2			{ get; private set; }
+		public int Level			{ get; private set; }
+		public int Price			{ get; private set; }
+		public byte RequiredRank	{ get; private set; }
+		public string Period		{ get; private set; }
+		public byte Damage			{ get; private set; }
+		public byte Radius			{ get; private set; }
+		public byte Range			{ get; private set; }
+		public int RequiredSP		{ get; private set; }
+		public int CoolDown			{ get; private set; }
+		public Skills Id			{ get; private set; }
 		//public Weapon[] Weapon
 	}
 }
